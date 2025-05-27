2a63d325a459a10bdee526792f8db7cf1fc22c02
avshyz <zarad.avishay@gmail.com>
2025-05-27 23:45:26 +0300

    replace rings with anuses

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -45,7 +45,7 @@ function AnimatedTorus({
 
   return (
     <mesh ref={meshRef} position={position} rotation={rotation} scale={scale}>
-      <torusGeometry />
+      <torusGeometry args={[0.5, 0.5, 32, 32]} />
       <meshNormalMaterial />
     </mesh>
   )
